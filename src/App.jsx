ddeae5169f9420a61ffc30d44fb8c507afea3dd9
vksaini-d <vikashsaini999999999@gmail.com>
2025-11-13 14:14:16 +0530

    stylesheet fix

--- src/App.jsx
+++ src/App.jsx
@@ -1346,3 +1346,9 @@ const ClockIcon = () => (
     <polyline points="22 4 12 14.01 9 11.01"></polyline>
   </svg>
 );
+
+const CheckIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <polyline points="20 6 9 17 4 12"></polyline>
+  </svg>
+);
